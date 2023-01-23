6e38af57748ac8470b5360c55b8f9fc345d9a0a1
uri <sfgtrainit@gmail.com>
2023-01-23 10:49:00 -0500

    update variable

--- variable_0.sh
+++ variable_0.sh
@@ -5,10 +5,10 @@
 
 ## ---------- variables allow to contain values that we want to use several times in our scripts -----------------
 
-#in our script, Serge is used 4 times, so good scripting practice requires us to create a variable to hold the Name. 
+#in our script, uri is used 4 times, so good scripting practice requires us to create a variable to hold the Name. 
 # If the name is no longer the same, we will only have to change the value of our variable
 
-name=Serge
+name=uri
 
 echo "$name is a very hard working guy"
 echo "The manger says, he will hire $name in his new team wherever he goes"
